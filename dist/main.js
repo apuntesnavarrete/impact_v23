32ca63bff0bb89f164821d126bde9bb85116e435
apuntesnavarrete <apuntesnavarrete@gmail.com>
2024-01-05 23:23:34 -0600

    modificacion de cors

--- dist/main.js
+++ dist/main.js
@@ -6,6 +6,11 @@ const common_1 = require("@nestjs/common");
 const swagger_1 = require("@nestjs/swagger");
 async function bootstrap() {
     const app = await core_1.NestFactory.create(app_module_1.AppModule);
+    app.enableCors({
+        origin: 'http://localhost:5173',
+        methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+        credentials: true,
+    });
     app.setGlobalPrefix("api/v1");
     app.useGlobalPipes(new common_1.ValidationPipe({
         whitelist: true,
--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,12 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.enableCors({
+    origin: 'http://localhost:5173', // Reemplaza con el dominio de tu aplicación frontend
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true, // Habilita el intercambio de cookies a través de las solicitudes de dominio cruzado
+  });
+
   app.setGlobalPrefix("api/v1");
 
 
